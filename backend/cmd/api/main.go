d71b10029e70cec71ae1ec85da86050179478391
vbdondarenko-cell <v.b.dondarenko@gmail.com>
2026-09-06 20:20:09 +0300

    Add Go API entrypoint

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/vbdondarenko-cell/qweqwe/backend/internal/httpserver"
+)
+
+func main() {
+	addr := os.Getenv("LINKUP_HTTP_ADDR")
+	if addr == "" {
+		addr = ":8080"
+	}
+
+	app := httpserver.New()
+	srv := &http.Server{
+		Addr:              addr,
+		Handler:           app.Handler(),
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       15 * time.Second,
+		WriteTimeout:      15 * time.Second,
+		IdleTimeout:       60 * time.Second,
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	errCh := make(chan error, 1)
+	go func() {
+		slog.Info("linkup api listening", "addr", addr)
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			errCh <- err
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+	case err := <-errCh:
+		slog.Error("http server failed", "error", err)
+		os.Exit(1)
+	}
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		slog.Error("graceful shutdown failed", "error", err)
+		os.Exit(1)
+	}
+}
