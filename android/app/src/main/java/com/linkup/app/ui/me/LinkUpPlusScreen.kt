2e2df1714b48462cb6e6fdd070245742ab9b433b
vbdondarenko-cell <v.b.dondarenko@gmail.com>
2026-09-07 18:54:00 +0300

    Fix LinkUp+ Compose scope import

--- android/app/src/main/java/com/linkup/app/ui/me/LinkUpPlusScreen.kt
+++ android/app/src/main/java/com/linkup/app/ui/me/LinkUpPlusScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
