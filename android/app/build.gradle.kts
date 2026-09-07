42aa3f157027fabbcf903c2d4a2fad1e007b2ae7
vbdondarenko-cell <v.b.dondarenko@gmail.com>
2026-09-07 15:39:37 +0300

    Use explicit Kotlin JUnit bridge for AGP 9 tests

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -143,7 +143,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.11.0")
 
-    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:2.4.10")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
