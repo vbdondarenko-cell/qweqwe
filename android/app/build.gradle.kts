4377a289489a84ca6ebc155fb47feeda8c26866d
vbdondarenko-cell <v.b.dondarenko@gmail.com>
2026-09-08 10:39:57 +0300

    Use JVM JSON implementation in Android unit tests

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -159,6 +159,7 @@ dependencies {
 
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:2.4.10")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.11.0")
+    testImplementation("org.json:json:20240303")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
